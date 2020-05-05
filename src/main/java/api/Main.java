82613c659c38fbd5a711c39d0b7e134633fa1c8e
Taha Hussain <tahahussain1994@gmail.com>
2020-05-04 21:13:19 -0400

    cahnges

--- src/main/java/api/Main.java
+++ src/main/java/api/Main.java
@@ -36,7 +36,7 @@ import transformers.ExceptionTransformer;
 public class Main {
 	public static void main(String[] args) {
 		//project end points
-		
+		 Spark.port(getHerokuAssignedPort());
 		setup();
 		
 //		Spark.get("/allProjects", (req, res) -> "Hello World");
@@ -68,6 +68,14 @@ public class Main {
 //		});
 		
 	}
+	
+	static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 	
 	public static void setup(){
 
